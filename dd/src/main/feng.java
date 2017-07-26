3ff4c163b3c8c20e8a4f8e0e6355df1630227e9b
wuhefeng <hohai_whf@163.com>
2017-07-27 01:00:49 +0800

    fff

---
+++ dd/src/main/feng.java
@@ -0,0 +1,14 @@
+package main;
+
+public class feng {
+   private int a;
+    private int b;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
